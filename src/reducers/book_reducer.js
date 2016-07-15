aeaa2ad6f558b81caddaa6a1999b86939eaa74e0
Rich Torres <richjtorres@gmail.com>
2016-07-14 17:55:31 -0700

    adding book reducer

---
+++ src/reducers/book_reducer.js
@@ -0,0 +1,10 @@
+export defaultfunction () {
+  return [
+    {
+      "title": "Pandora's Start",
+      "title": "The Unbearable Lightness of Being".
+      "title": "Gilead",
+      "title": "Jarassic Park"
+    }
+  ]
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './book_reducer.js'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
