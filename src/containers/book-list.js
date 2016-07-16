51e1663b882d7f81b00318e37581bcff7917a8ac
Rich Torres <richjtorres@gmail.com>
2016-07-16 12:59:17 -0700

    working on first redux action

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
@@ -15,16 +17,20 @@ class BookList extends Component {
   render() {
     return (
       <ul className='list-group col-sm-4'>
-        { this.renderList() }
+        {this.renderList()}
       </ul>
-    )  
+    );
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
     books: state.books
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToPropos(dispatch) {
+  return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToPropos)(BookList);
