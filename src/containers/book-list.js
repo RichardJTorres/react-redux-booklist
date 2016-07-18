45203e3bc3127d13d8ae03260f11b94052901e1a
Rich Torres <richjtorres@gmail.com>
2016-07-17 19:13:46 -0700

    fixing onClick function call

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -7,7 +7,10 @@ class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className='list-group-item'>
+        <li
+          onClick={() => this.props.selectBook(book)}
+          key={book.title}
+          className='list-group-item'>
          {book.title}
         </li>
       );
@@ -29,8 +32,8 @@ function mapStateToProps(state) {
   };
 }
 
-function mapDispatchToPropos(dispatch) {
+function mapDispatchToProps(dispatch) {
   return bindActionCreators({ selectBook: selectBook}, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToPropos)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
